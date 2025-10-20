3b73a6612f387d1abe39fc6f7cfa8422e2a5e458
mazin0eg <mazin.zougui@gmail.com>
2025-10-20 18:23:52 +0100

    feat: server connection with express(ts)

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+
+const app = express();
+const port: number = 3000;
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});

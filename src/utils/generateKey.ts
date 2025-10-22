011028901763a62c439a6c2df64411db4f035ff3
Mohammed-Ben-Cheikh <bencheikh.official@gmail.com>
2025-10-22 16:00:53 +0000

    refactor: enhance type definitions in generateKey function and clean up code formatting

--- src/utils/generateKey.ts
+++ src/utils/generateKey.ts
@@ -1,11 +1,11 @@
 import { randomInt } from "crypto";
 
-export const generateKey = (blocks = 5, blockLen = 5) => {
-  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  let parts = [];
+export const generateKey = (blocks: number = 5, blockLen: number = 5) => {
+  const alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let parts: string[] = [];
   for (let i = 0; i < blocks; i++) {
-    let part = "";
-    for (let j = 0; j < blockLen; j++) {
+    let part: string = "";
+    for (let j: number = 0; j < blockLen; j++) {
       part += alphabet[Math.floor(randomInt(alphabet.length))];
     }
     parts.push(part);

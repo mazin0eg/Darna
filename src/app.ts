8284735c262d5ebaf231970df6dbf7ac709ebe45
dounia-elg <dounia.elg20@gmail.com>
2025-10-23 14:41:28 +0100

    DAR-102 Création d’une entreprise par un particulier inscrit

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
 import "dotenv/config";
 import express from "express";
 import { connectDB } from "./config/dbconfig";
+import { responseHandler } from "./middleware/responseHandler";
 
 import authRoutes from "./routes/auth";
+import entrepriseRoutes from "./routes/entreprise";
 
 const app = express();
 connectDB();
 app.use(express.json());
+app.use(responseHandler);
 
 app.use("/api/auth", authRoutes);
+app.use("/api/entreprises", entrepriseRoutes);
 
 export default app;

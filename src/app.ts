4ceafc2e7710485e62d641b3c9c335d4096007a6
mazin0eg <mazin.zougui@gmail.com>
2025-10-23 16:12:49 +0100

    bug: fix app import conflits

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,10 @@ import { responseHandler } from "./middleware/responseHandler";
 
 import { validatorHandler } from "./middleware/validatorHamdler";
 import authRoutes from "./routes/auth";
-<<<<<<< HEAD
+
 import entrepriseRoutes from "./routes/entreprise";
-=======
-import { responseHandler } from "./middleware/responseHandler";
->>>>>>> fe5a1613140a62e6749b39add7d325d4d3c127cb
+
+
 
 const app = express();
 connectDB();

3294cf26792acbe7f685d405c91bb2ac656f09e4
dounia-elg <dounia.elg20@gmail.com>
2025-10-27 17:04:00 +0100

    add get methodes to get entreprises and employees data

--- src/routes/entreprise.ts
+++ src/routes/entreprise.ts
@@ -9,13 +9,18 @@ import {
 } from "../validator/entrepriseValidator";
 const router = express.Router();
 
-
+router.get("/", authenticate, EntrepriseController.getEntreprises);
+router.get("/:id", authenticate, EntrepriseController.getEntreprise);
 router.post("/", authenticate, createValidators, EntrepriseController.create);
 router.patch("/:id", authenticate, updateValidators, EntrepriseController.update);
 router.delete("/:id", authenticate, EntrepriseController.remove);
+
+router.get("/:id/employees", authenticate, EntrepriseController.getEmployees);
+router.get("/:id/employees/:employeeId", authenticate, EntrepriseController.getEmployee);
 router.post("/:id/employees", authenticate, addEmployeeValidators, EntrepriseController.addEmployee);
 router.patch("/:id/employees/:employeeId", authenticate, updateEmployeeValidators, EntrepriseController.updateEmployee);
 router.delete("/:id/employees/:employeeId", authenticate, EntrepriseController.removeEmployee);
+
 	
 
 export default router;
